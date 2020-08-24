63cb074ae3d5a45ce42ee4a0b40e12d48999e6b5
Drew Scott <ascott3312@gmail.com>
2020-08-24 18:08:59 -0500

    Add Reverse Array

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -3,10 +3,22 @@
 namespace Challenges
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arrayToReverse = new[] { 1, 2, 3, 4, 5 };
+            int[] reversed = ReverseArray(arrayToReverse);
+            Console.WriteLine(String.Join(",", reversed));
         }
-    }
+        public static int[] ReverseArray(int[] array)
+        {
+            int[] result = new int[array.Length];
+            for (int i = 1; i < array.Length + 1; i++)
+            {
+                result[i-1] = array[array.Length - i];
+            }
+
+            return result;
+        }
+    } 
 }
