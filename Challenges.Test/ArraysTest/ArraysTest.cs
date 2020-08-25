ed9e3d661703ebbe333877ac6593c6f67765eb2c
Drew Scott <ascott3312@gmail.com>
2020-08-25 14:11:38 -0500

    Code Challenges

---
+++ Challenges.Test/ArraysTest/ArraysTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Challenges.Test.ArraysTest
+
+{
+    public class ArraysTest
+    {
+        public ArraysTest()
+        {
+                int[] arrayToReverse = new[] { 1, 2, 3, 4, 5 };
+                int[] reversed = ReverseArray(arrayToReverse);
+                Console.WriteLine(String.Join(",", reversed));
+            }
+        
+    }
+}
---
+++ Challenges/Arrays/Reverse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Challenges.Arrays
+{
+    public class Reverse
+    {
+        public static int[] ReverseArray(int[] array)
+        {
+            int[] result = new int[array.Length];
+            for (int i = 1; i < array.Length + 1; i++)
+            {
+                result[i - 1] = array[array.Length - i];
+            }
+
+            return result;
+        }
+    }
+}
