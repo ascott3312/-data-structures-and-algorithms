085d674206aea673f90ddd37f95a93359e7fb6fa
Drew Scott <ascott3312@gmail.com>
2020-08-28 21:41:48 -0500

    Add Linked List Code

--- DataStructures.Tests/UnitTest1.cs
+++ DataStructures.Tests/UnitTest1.cs
@@ -1,14 +1,37 @@
 using System;
 using Xunit;
 
-namespace DataStructures.Tests
+namespace DataStructures.Tests.LinkedLists
 {
-    public class UnitTest1
+    public class LinkedListTests
     {
         [Fact]
-        public void Test1()
+        public void LinkedList_starts_empty()
         {
+            // Act
+            LinkedList list = new LinkedList();
 
+            // Assert
+            Assert.Equal("NULL", list.ToString());
+        }
+
+        [Fact(Skip = "TODO for students")]
+        public void Insert_adds_value_to_start()
+        {
+            // Arrange
+            LinkedList list = new LinkedList();
+
+            // Act
+            list.Insert(1);
+
+            // Assert
+            Assert.Equal("{ 1 } -> NULL", list.ToString());
+
+            // Act
+            list.Insert(2);
+
+            // Assert
+            Assert.Equal("{ 2 } -> { 1 } -> NULL", list.ToString());
         }
     }
 }
