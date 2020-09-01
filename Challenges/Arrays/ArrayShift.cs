c8d1425d4a5c5ff69066ee7ec9ec1991b79bc08f
Drew Scott <ascott3312@gmail.com>
2020-08-31 21:58:38 -0500

    trying fix errors

--- Challenges/Arrays/ArrayShift.cs
+++ Challenges/Arrays/ArrayShift.cs
@@ -6,17 +6,7 @@ namespace Challenges.Arrays
     {
         public ArrayShift
        
-          (ArrayShift [0] = 2;
-          ArrayShift [1] = 4;
-          ArrayShift [2] = 6;
-          ArrayShift [3] = 8;
-          ArrayShift [4] = 5;),
-
-          (ArrayShift[0] = 4;
-          ArrayShift[1] = 8;
-          ArrayShift[2] = 23;
-          ArrayShift[3] = 42;
-          ArrayShift[4] = 15;)
           
+   
        
     }
